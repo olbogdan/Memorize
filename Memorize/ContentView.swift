045f1658cd62895813a35704d1927761d1cdbad0
bogdanov <iceposhta@gmail.com>
2021-04-13 13:40:43 +0100

    initial commit

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,8 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        return HStack {
+            ForEach(0 ..< 4) { _ in
+                CardView(isFaceUp: true)
+                    .padding()
+                    .foregroundColor(Color.orange)
+                    .font(.largeTitle)
+            }
+        }
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool
+
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text("b")
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
     }
 }
 
