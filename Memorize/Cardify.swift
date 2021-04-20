7937265ba6172afea095896469742db6c113ef86
bogdanov <iceposhta@gmail.com>
2021-04-21 00:06:24 +0100

    create a ViewModifiler Cardify for conditional cards drawing

---
+++ Memorize/Cardify.swift
@@ -0,0 +1,33 @@
+//
+//  Cardify.swift
+//  Memorize
+//
+//  Created by bogdanov on 19.04.21.
+//
+
+import SwiftUI
+
+struct Cardify: ViewModifier {
+    var isFaceUp: Bool
+
+    func body(content: Content) -> some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
+                RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
+                content
+            } else {
+                RoundedRectangle(cornerRadius: cornerRadius).fill()
+            }
+        }
+    }
+
+    private let cornerRadius: CGFloat = 10
+    private let edgeLineWidth: CGFloat = 3
+}
+
+extension View {
+    func cardify(isFaceUp: Bool) -> some View {
+        modifier(Cardify(isFaceUp: isFaceUp))
+    }
+}
